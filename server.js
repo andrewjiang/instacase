bf18730fae4a667010b05ccc54b81fe3cd6f20bf
Zernyu Chou <swiftdemise@gmail.com>
2014-03-17 14:44:10 -0500

    Added Mongo config. Created User and Address models. User is created on FB login

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 /*
   TODO: use this layout: https://github.com/madhums/node-express-mongoose-demo
-  TODO: break out config into config files
+  TODO: break out config into config files etc
 */
 
 var debug = require('debug')('instacase');
 
+// Import server-related libraries
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -13,6 +14,50 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Import utility libraries
+var _ = require('underscore');
+var validator = require('validator');
+
+debug('Booting Instacase');
+
+// Set up the DB
+var mongoose = require('mongoose'); // Mongo ODM
+var MONGO_URL = process.env.MONGOHQ_URL || 'mongodb://localhost/instacase';
+debug('Connecting to MongoDB at ' + MONGO_URL);
+
+mongoose.connect(MONGO_URL, function(err, res) {
+    if (err) {
+        console.error('Could not connect to MongoDB at ' + MONGO_URL + '. ' + err);
+    } else {
+        debug('Connected to MongoDB at ' + MONGO_URL);
+    }
+});
+
+// Set up models
+var Schema = mongoose.Schema;
+var userSchema = Schema({
+    name: {
+        first: { type: String, trim: true },
+        last: { type: String, trim: true}
+    },
+    email: { type: String, trim: true, required: true},
+    address: [{ type: Schema.Types.ObjectId, ref: 'Address' }]
+});
+
+var addressSchema = Schema({
+    name: { type: String, required: true },
+    line_one: { type: String, required: true },
+    line_two: String,
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipCode: { type: String, required: true }
+});
+
+var User = mongoose.model('Users', userSchema);
+var Address = mongoose.model('Address', addressSchema);
+
+
+// Facebook OAUTH config
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var FACEBOOK_APP_ID = '236858273165593';
@@ -20,7 +65,7 @@ var FACEBOOK_APP_SECRET = '1a1c8584fd3ed34796f39ba5f826d4d7';
 var FACEBOOK_OAUTH_CALLBACK_URL =
         (process.env.NODE_ENV === 'production'
             ? 'http://instacase.herokuapp.com/auth/facebook/callback'
-            : 'http://localhost:8080/auth/facebook/callback');
+            : 'http://localhost:3000/auth/facebook/callback');
 
 var routes = require('./routes');
 var users = require('./routes/user');
@@ -45,7 +90,7 @@ passport.deserializeUser(function(obj, done) {
     done(null, obj);
 });
 
-// Set up facebook auth
+// Set up facebook auth bindings
 
 // Use the FacebookStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
@@ -88,6 +133,31 @@ app.use(passport.session());
 app.use(app.router);
 
 
+// Util methods
+function authUser(user) {
+    var emails = _.map(user.emails, function(email) { return email.value; });
+    User.find({ email: { $in: emails } })
+        .exec(_.partial(function(userInfo, err, users) {
+            if (users.length === 0) {
+                var newUser = new User({
+                    name: {
+                        first: userInfo.name.givenName,
+                        last: userInfo.name.familyName
+                    },
+                    email: userInfo.emails[0].value
+                });
+
+                newUser.save(function(err, user) {
+                    if (err) {
+                        // TODO: pass this onto the frontend?
+                        return console.error(err);
+                    }
+                });
+            }
+        }, user));
+}
+
+
 // Views
 
 app.get('/', routes.index);
@@ -103,7 +173,7 @@ app.get('/user/logout', users.logout);
 //   redirect the user back to this application at /auth/facebook/callback
 app.get('/auth/facebook',
     passport.authenticate('facebook', { scope: ['basic_info', 'email'] })
-); // TODO: do we need basic info? or just email address
+);
 
 // GET /auth/facebook/callback
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -111,8 +181,16 @@ app.get('/auth/facebook',
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', { successRedirect: '/',
-                                        failureRedirect: '/login' })
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+
+    // Successful authentication.
+    function(req, res) {
+        // Check for user account
+        authUser(req.user);
+
+        // Redirect home
+        res.redirect('/');
+    }
 );
 
 
@@ -158,6 +236,7 @@ function ensureAuthenticated(req, res, next) {
 
 
 app.set('port', process.env.PORT || 3000);
+debug('Setting port to ' + app.get('port'));
 
 // TODO: should this be exposed?
 var server = app.listen(app.get('port'), function() {
