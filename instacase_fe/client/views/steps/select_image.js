3c01f73e0b414d2b5bf9d03457e78c30f7b64138
Andrew Jiang <andrew.h.jiang@gmail.com>
2014-03-18 08:56:25 -0700

    Changes

--- instacase_fe/client/views/steps/select_image.js
+++ instacase_fe/client/views/steps/select_image.js
@@ -1,67 +1,75 @@
 Template.select_image.rendered = function(){
+	document.getElementById('image-input').addEventListener('change', newUpload, false);
+  
+  // Obtain a canvas drawing surface from fabric.js
+  window.canvas = new fabric.Canvas('insta-canvas');
+  canvas.setHeight($('#canvas-container').height());
+	canvas.setWidth($('#canvas-container').width());
 
-        
-        // Obtain a canvas drawing surface from fabric.js
-        var canvas = new fabric.Canvas('insta-canvas');
-        canvas.setHeight($('#canvas-container').height());
-				canvas.setWidth($('#canvas-container').width());
- 
-        // Create a text object. 
-        // Does not display it-the canvas doesn't 
-        // know about it yet.
-        var hi = new fabric.Text('hello, world.', {
-            left: canvas.getWidth() / 2,
-            top: canvas.getHeight() / 2        
-        });
+  // Create a text object. 
+  // Does not display it-the canvas doesn't 
+  // know about it yet.
+  /* var hi = new fabric.Text('hello, world.', {
+      left: canvas.getWidth() / 2,
+      top: canvas.getHeight() / 2        
+  }); */
 
-        /*var rect = new fabric.Rect({
-				  left: canvas.getWidth() / 2,
-          top: canvas.getHeight() / 2,   
-          stroke: 'red',
-				  fill: 'none',
-				  width: 210,
-				  height: 420,
-				  rx: 20,
-				  ry: 20,
-				  selectable: true,
-				});*/
+  /*var rect = new fabric.Rect({
+	  left: canvas.getWidth() / 2,
+    top: canvas.getHeight() / 2,   
+    stroke: 'red',
+	  fill: 'none',
+	  width: 210,
+	  height: 420,
+	  rx: 20,
+	  ry: 20,
+	  selectable: true,
+	});*/
 
-				/*var rect2 = new fabric.Rect({
-				  left: canvas.getWidth() / 2,
-          top: canvas.getHeight() / 2,   
-          stroke: 'black',
-				  fill: 'none',
-				  width: 254,
-				  height: 464,
-				  rx: 36,
-				  ry: 36,
-				  selectable: true
-				});*/
+	/*var rect2 = new fabric.Rect({
+	  left: canvas.getWidth() / 2,
+    top: canvas.getHeight() / 2,   
+    stroke: 'black',
+	  fill: 'none',
+	  width: 254,
+	  height: 464,
+	  rx: 36,
+	  ry: 36,
+	  selectable: true
+	});*/
 
-				var cwidth = canvas.getWidth() / 2 - 500;
-				var cheight = canvas.getHeight() / 2 - 500;
+	var cwidth = canvas.getWidth() / 2 - 500;
+	var cheight = canvas.getHeight() / 2 - 500;
+	var iwidth =  canvas.getWidth() / 2 
+	var iheight	= canvas.getHeight() / 2 
 
-        fabric.Image.fromURL('http://agileimpact.org/wp-content/uploads/2014/03/TwWc21Ai.jpeg', function(oImg) {
-				  canvas.add(oImg);
-				});
+  fabric.Image.fromURL('http://agileimpact.org/wp-content/uploads/2014/03/TwWc21Ai.jpeg', function(oImg) {
+	  canvas.add(oImg), {left: iwidth, top: iheight}
+	});
 
-				canvas.setOverlayImage('/images/overlay.png', 
-					canvas.renderAll.bind(canvas), 
-					{overlayImageLeft: cwidth, overlayImageTop: cheight});
-				
+	canvas.setOverlayImage('/images/overlay.png', 
+		canvas.renderAll.bind(canvas), 
+		{overlayImageLeft: cwidth, overlayImageTop: cheight});
+	
 
-    
-        // Attach it to the canvas object, then (re)display
-        // the canvas.    
-        canvas.add(hi);
 
-        canvas.add(rect2);
-        canvas.add(rect);
-        canvas.sendBackward(oImg);
+  // Attach it to the canvas object, then (re)display
+  // the canvas.    
+  // canvas.add(hi);
+
+  //canvas.add(rect2);
+  //canvas.add(rect);
+  canvas.sendBackward(oImg);
         
 };
 Template.select_image.events({
 	'click #upload-icon': function(event){
 		$('#image-input').click();
-	},
+	}
 });
+function newUpload(evt) {
+	URL = 'http://agileimpact.org/wp-content/uploads/2014/03/TwWc21Ai.jpeg'
+  fabric.Image.fromURL(URL, function(oImg) {
+	  canvas.add(oImg), {left: 100, top: 100}
+	});
+}
