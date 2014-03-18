d6a2a1edb91ce721a8359f704bc390019bd4dabd
Andrew Jiang <andrew.h.jiang@gmail.com>
2014-03-18 11:24:01 -0700

    More fix

--- instacase_fe/client/helpers/router.js
+++ instacase_fe/client/helpers/router.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 Meteor.Router.add({
 	'/': 'home',
 	'/select': 'select_image',
 	'/choose': 'choose_model',
 	'/canvas': 'canvas'
-=======
-Meteor.Router.add({
-	'/': 'home',
-	'/select': 'select_image',
-	'/choose': 'choose_model'
->>>>>>> 349f21351392d0cb44681c3fccb624836f0afa80
 });
