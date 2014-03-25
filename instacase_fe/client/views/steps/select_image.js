c88cc38306e2d6b1f4ba663e412d84264c643861
Andrew Jiang <andrew.h.jiang@gmail.com>
2014-03-25 16:19:09 -0700

    CSS changes and some feature adds'

--- instacase_fe/client/views/steps/select_image.js
+++ instacase_fe/client/views/steps/select_image.js
@@ -3,10 +3,10 @@ Template.select_image.rendered = function(){
 
 
 	if(jQuery.browser.mobile) {
-	 	window.OverlayImg = '/images/overlay_m.png';
+	 	//window.OverlayImg = '/images/overlay_m.png';
 	 	window.isMobile = true;
 	} else {
-		window.OverlayImg = '/images/overlay.png';
+		//window.OverlayImg = '/images/overlay.png';
 		window.isMobile = false;
 	};
 
@@ -35,38 +35,86 @@ Template.select_image.rendered = function(){
       top: canvas.getHeight() / 2        
   }); */
 
-  /*var rect = new fabric.Rect({
-	  left: canvas.getWidth() / 2,
-    top: canvas.getHeight() / 2,   
-    stroke: 'red',
-	  fill: 'none',
-	  width: 210,
-	  height: 420,
-	  rx: 20,
-	  ry: 20,
-	  selectable: true,
-	});*/
-
-	/*var rect2 = new fabric.Rect({
-	  left: canvas.getWidth() / 2,
-    top: canvas.getHeight() / 2,   
-    stroke: 'black',
-	  fill: 'none',
-	  width: 254,
-	  height: 464,
-	  rx: 36,
-	  ry: 36,
-	  selectable: true
-	});*/
+  var rect = new fabric.Rect({
+	  originY: "top",
+	  left: canvas.getWidth() / 2+1,
+    top: 20,  
+	  fill: 'white',
+	  width: 250,
+	  height: 460,
+	  rx: 46,
+	  ry: 46,
+	  selectable: false,
+	});
 
 	if (isMobile){
-		var cwidth = canvas.getWidth() / 2 - 960;
-		var cheight = canvas.getHeight() / 2 - 540;
+
 	} else {
-		var cwidth = canvas.getWidth() / 2 - 1000;
-		var cheight = canvas.getHeight() / 2 - 500;
+
 	}
 	
+	//Center Overlay Divs
+
+	var cWidth = canvas.getWidth();
+	var cHeight = canvas.getHeight();
+
+	var outsideW = $('#outside-border').width();
+	var outsideH = $('#outside-border').width();
+	var insideW = $('#inside-border').width();
+	var insideH = $('#inside-border').width();
+
+	$('#outside-edge').css('left', cWidth/2 - outsideW/2);
+	$('#outside-border').css('left', cWidth/2 - outsideW/2);
+	$('#inside-border').css('left', cWidth/2 - insideW/2);
+
+	$("#colorpicker").spectrum({
+
+
+
+	    color: "#111",
+	    flat: true,
+    	showInput: true,
+    	clickoutFiresChange: true,
+    	showAlpha: true,
+    	showPalette: true,
+	    palette: [
+	        ['black', 'white', 'blanchedalmond'],
+	        ['rgb(255, 128, 0);', 'hsv 100 70 50', 'lightyellow']
+	    ],
+	    showButtons: false,
+	    preferredFormat: "hex",
+	    move: function(color) {
+	    	var obj = canvas.getActiveObject();
+    		var color = color.toRgbString();
+    		obj.fill = color;
+    		canvas.renderAll();
+    		$('#font-textarea').css('color', color);
+			}
+
+	});
+
+	$("#bkg-colorpicker").spectrum({
+
+
+
+	    color: "#fff",
+	    flat: true,
+    	showInput: true,
+    	clickoutFiresChange: true,
+    	showPalette: true,
+	    palette: [
+	        ['black', 'white', 'blanchedalmond'],
+	        ['rgb(255, 128, 0);', 'hsv 100 70 50', 'lightyellow']
+	    ],
+	    showButtons: false,
+	    preferredFormat: "hex",
+	    move: function(color) {
+    		var color = color.toRgbString();
+    		rect.fill = color;
+    		canvas.renderAll();
+			}
+
+	});
 
   /*fabric.Image.fromURL('http://agileimpact.org/wp-content/uploads/2014/03/TwWc21Ai.jpeg', function(oImg) {
 	  canvas.add(oImg), {left: iwidth, top: iheight}
@@ -74,39 +122,68 @@ Template.select_image.rendered = function(){
 	
 	
 	
-	canvas.setOverlayImage(OverlayImg, 
+	/*canvas.setOverlayImage(OverlayImg, 
 		canvas.renderAll.bind(canvas), 
-		{overlayImageLeft: cwidth, overlayImageTop: cheight});
+		{overlayImageLeft: cwidth, overlayImageTop: cheight});*/
 	
 
 
   // Attach it to the canvas object, then (re)display
   // the canvas.    
   // canvas.add(hi);
-
-  //canvas.add(rect2);
-  //canvas.add(rect);
+  canvas.add(rect);
   //canvas.sendBackward(oImg);
+
+  $('#clipart-box img').click(function(){
+  	var imgElement = this;
+		var image = new fabric.Image(imgElement, {
+		  left: 100,
+		  top: 100,
+		});
+		canvas.add(image);
+		image.center();
+		image.setCoords();
+		canvas.setActiveObject(image);
+  });
+
   canvas.on('object:selected', function(options) {
 	  var obj = canvas.getActiveObject();
+	  $('#options-box').removeClass("hidden");
 	  try{
 	  	switch(obj.type)
 		  {
-		  	case "image":
-		  	  $('#image-box').removeClass("hidden");
-		  	  break;
 		  	case "text":
-		  	  $('#text-box').removeClass("hidden");
+		  		$('#clipart-box').addClass('hidden');
+		  		$('#background-box').addClass('hidden');
 		  	  $('#font-box').removeClass("hidden");
 		  	  getFontInfo();
 		  	  break;
+		  	case "image":
+		  	  $('#font-box').addClass("hidden");
+		  	  break;
 		  	default:
-		  		$('#image-box').removeClass("hidden");
+		  		$('#font-box').addClass("hidden");
 		  	  break;
 		  };
 	  }
 	  catch(err){}
-	  
+
+	  var index = canvas.getObjects().indexOf(obj);
+	  var numIndex = canvas.getObjects().length - 1;
+
+	  if(index<numIndex && index>1){
+	  	$('#move-up-icon').removeClass("hidden");
+	  	$('#move-down-icon').removeClass("hidden");
+	  } else if (index==numIndex && index>1){
+	  	$('#move-up-icon').addClass("hidden");
+	  	$('#move-down-icon').removeClass("hidden");
+	  } else if (index==1 && index<numIndex){
+	  	$('#move-up-icon').removeClass("hidden");
+	  	$('#move-down-icon').addClass("hidden");
+	  } else{
+	  	$('#move-up-icon').addClass("hidden");
+	  	$('#move-down-icon').addClass("hidden");
+	  };
 	  
 	  moveButtons();
 
@@ -140,13 +217,22 @@ Template.select_image.rendered = function(){
 	canvas.on('selection:cleared', function(options) {
 	  var obj = canvas.getActiveObject();
 
-	  $('#image-box').addClass("hidden");
-	  
-	  $('#text-box').addClass("hidden");
+	  $('#options-box').addClass("hidden");
 	  $('#font-box').addClass("hidden");
+	  $('#move-up-icon').addClass("hidden");
+	  $('#move-down-icon').addClass("hidden");
 
-	  
 	});
+
+
+	//listen for text area changes and change font with it
+	$('#font-textarea').bind('input propertychange', function() {
+		var obj = canvas.getActiveObject();
+		obj.text = $('#font-textarea').val();
+		canvas.renderAll();
+		moveButtons();
+	});
+
 };
 
 Template.select_image.events({
@@ -175,35 +261,108 @@ Template.select_image.events({
 	  var cCenLeft = (canvas.getWidth() + 0.0) / 2;
 		var cCenTop = (canvas.getHeight() + 0.0) / 2;
 
-		var newText = window.prompt("Your text","");
-
-		var text = new fabric.Text(newText, { left: cCenLeft, top: cCenTop });
-		canvas.setActiveObject(text);
+		var text = new fabric.Text("", { 
+			left: cCenLeft, 
+			top: cCenTop,
+			fontFamily: "Arial",
+			fontSize: 24,
+			lockUniScaling: true
+		});
 		canvas.add(text);
+		canvas.setActiveObject(text);
+		showEditor();
+		$('#font-textarea').focus();
 
 	},
 	'click #edit-icon': function(event){
 		
-		var obj = canvas.getActiveObject(); 
-
-	  var cCenLeft = (canvas.getWidth() + 0.0) / 2;
-		var cCenTop = (canvas.getHeight() + 0.0) / 2;
-
-		var newText = window.prompt("Your text","");
-
-		var text = new fabric.Text(newText, { left: cCenLeft, top: cCenTop });
-		canvas.setActiveObject(text);
-		canvas.add(text);
-
-
-		/*var offL = text.left - cCenLeft;
-		var offT = text.top - cCenTop; 
-
-		text.offLeft = offL;
-		text.offTop = offT;
-		json = JSON.stringify(canvas.toJSON(['offTop', 'offLeft']));*/
+		showEditor();
 
 	},
+	'click #align-left': function(event){
+		var obj = canvas.getActiveObject(); 
+		obj.textAlign = "left";
+		canvas.renderAll();
+		getFontInfo();
+		$('#align-left').addClass('chosen');
+		$('#align-center').removeClass('chosen');
+		$('#align-right').removeClass('chosen');
+
+	},
+	'click #align-center': function(event){
+		var obj = canvas.getActiveObject(); 
+		obj.textAlign = "center";
+		canvas.renderAll();
+		getFontInfo();
+		$('#align-left').removeClass('chosen');
+		$('#align-center').addClass('chosen');
+		$('#align-right').removeClass('chosen');
+	},
+	'click #align-right': function(event){
+		var obj = canvas.getActiveObject(); 
+		obj.textAlign = "right";
+		canvas.renderAll();
+		getFontInfo();
+		$('#align-left').removeClass('chosen');
+		$('#align-center').removeClass('chosen');
+		$('#align-right').addClass('chosen');
+	},
+	'click #clipart-box img': function(event, object){
+		
+	},
+	'change #font-selector': function(event){
+		var obj = canvas.getActiveObject(); 
+		obj.fontFamily = $('#font-selector').val();
+		canvas.renderAll();
+		getFontInfo();
+	},
+	'click #clipart-icon': function(event){
+		$('#font-box').addClass('hidden');
+		$('#clipart-box').removeClass('hidden');
+		$('#background-box').addClass('hidden');
+	},
+	'click #background-icon': function(event){
+		$('#font-box').addClass('hidden');
+		$('#clipart-box').addClass('hidden');
+		$('#background-box').removeClass('hidden');
+	},
+	'click .close-button': function(event){
+		$('#clipart-box').addClass('hidden');
+		$('#background-box').addClass('hidden');
+		$('#font-box').addClass('hidden');
+	},
+	'click #order-edges': function(event){
+		$('#outside-edge').toggleClass('hidden')
+	},
+	'click #move-up-icon': function(event){
+		var obj = canvas.getActiveObject();
+	  var numIndex = canvas.getObjects().length - 1;
+	  canvas.moveTo(obj, numIndex);
+	},
+	'click #move-down-icon': function(event){
+		var obj = canvas.getActiveObject();
+	  var index = canvas.getObjects().indexOf(obj);
+	  canvas.moveTo(obj, 1);
+	}
+	/*'change #color-selector': function(event){
+		var obj = canvas.getActiveObject(); 
+
+		var color = $('#color-selector').val();
+
+		var rgbcolor = hexToRgb(color);
+
+		obj.fill = rgbcolor;
+
+		canvas.renderAll();
+
+		getFontInfo;
+		
+
+		//document.getElementById('color-selector').color.fromString(hexcolor);
+		//obj.fontFamily = $('#font-selector').val();
+		//canvas.renderAll();
+		//getFontInfo();
+	},*/
 });
 
 function newUpload(evt) {
@@ -225,7 +384,8 @@ function newUpload(evt) {
 				left: 0,
 				top: 0,
 				offLeft: 0,
-				offTop: 0
+				offTop: 0,
+				angle: 0
 			});
 			image.set({
 				scaleY: 260 / (image.width),
@@ -251,37 +411,16 @@ function moveButtons(){
 	var r = Math.pow(Math.pow(rX, 2) + Math.pow(rY, 2), 0.5) + 20;
 	var theta = ((obj.angle) * Math.PI / 180) + Math.atan(rY/rX);
 
-  var IconX = obj.left - r * Math.cos(theta) - 12;
-  var IconY = obj.top - r * Math.sin(theta) - 34;
+	var optionsH = $('#options-box').height();
+	var optionsW = $('#options-box').width();
 
-	try{
-		switch(obj.type)
-	  {
-	  	case "image":
-	  	  $('#image-box').css("left", IconX);
-		  	$('#image-box').css("top", IconY);
-		  	$('#image-box').css("transform", "rotate(" + obj.angle + "deg)");
-	  	  break;
-	  	case "text":
-	  	  $('#text-box').css("left", IconX);
-		  	$('#text-box').css("top", IconY);
-		  	$('#text-box').css("transform", "rotate(" + obj.angle + "deg)");
-	  	  break;
-	  	default:
-	  		$('#text-box').css("left", IconX);
-		  	$('#text-box').css("top", IconY);
-		  	$('#text-box').css("transform", "rotate(" + obj.angle + "deg)");
-	  	  break;
-	  };
-	}
-	catch(err){
-		$('#text-box').css("left", IconX);
-  	$('#text-box').css("top", IconY);
-  	$('#text-box').css("transform", "rotate(" + obj.angle + "deg)");
-	}
- 
+  var IconX = obj.left - r * Math.cos(theta) - optionsW/2;
+  var IconY = obj.top - r * Math.sin(theta) - optionsH/2;
 
-  console.log(obj);
+  $('#options-box').css("left", IconX);
+	$('#options-box').css("top", IconY);
+	$('#options-box').css("transform", "rotate(" + obj.angle + "deg)");
+
 };
 function resizeCanvas(){
 	canvas.loadFromJSON(json);
@@ -295,48 +434,79 @@ function resizeCanvas(){
 	canvas.setWidth($('#canvas-container').width());
 
 	//setting OverLay Image
-	canvas.overlayImage = null;
+	//canvas.overlayImage = null;
 
 
 	//set Canvas Height and Width
 	cWidth = canvas.getWidth() + 0.0;
 	cHeight = canvas.getHeight() + 0.0;
+	var rect = canvas.item(0);
 	var oWidth = cWidth / 2 - 1000;
 	var oHeight = cHeight / 2 - 500;
 	var cCenterTop = cHeight / 2;
 	var cCenterLeft = cWidth / 2;
 
-	/*objects = JSON.parse(JSON.stringify(canvas));
+	var outsideW = $('#outside-border').width();
+	var outsideH = $('#outside-border').width();
+	var insideW = $('#inside-border').width();
+	var insideH = $('#inside-border').width();
 
-	for (var i=0; i <= objects.objects.length-1; i++){
-		var nTop = cCenterTop + canvas.item(i).offTop;
-		var nLeft = cCenterLeft + canvas.item(i).offLeft;
-
-		canvas.item(i).set({
-			top: nTop,
-			left: nLeft,
-		});
-	};
-
-	json = JSON.stringify(canvas.toJSON(['offTop', 'offLeft']));*/
-	
-
-	canvas.setOverlayImage(OverlayImg, 
+	$('#outside-border').css('left', cWidth/2 - outsideW/2);
+	$('#inside-border').css('left', cWidth/2 - insideW/2);
+	$('#outside-edge').css('left', cWidth/2 - outsideW/2);
+	rect.set({
+		left: cCenterLeft,
+	});
+	canvas.renderAll();
+	/*canvas.setOverlayImage(OverlayImg, 
 		canvas.renderAll.bind(canvas), 
-		{overlayImageLeft: oWidth, overlayImageTop: oHeight});
+		{overlayImageLeft: oWidth, overlayImageTop: oHeight});*/
 
 };
 function getFontInfo(){
+
 	var obj = canvas.getActiveObject();
-	var font = obj.fontFamily;
+	var family = obj.fontFamily;
 	var text = obj.text;
-	var text = obj.text;
-	$('#font-selector').val(font);
+	var align = obj.textAlign;
+	var color = obj.fill;
+
+	/*var color = obj.fill.match(/\((\d+),(\d+),(\d+)\)/);
+	var hexcolor = rgbToHex(color[1], color[2], color[3]);
+	document.getElementById('color-selector').color.fromString(hexcolor);*/
+
+	$('#font-selector').val(family);
+	
+	switch(align)
+	{
+		case "left":
+			$('#align-left').addClass('chosen');
+			$('#align-center').removeClass('chosen');
+			$('#align-right').removeClass('chosen');
+			break;
+		case "center":
+			$('#align-left').removeClass('chosen');
+			$('#align-center').addClass('chosen');
+			$('#align-right').removeClass('chosen');
+			break;
+		case "right":
+			$('#align-left').removeClass('chosen');
+			$('#align-center').removeClass('chosen');
+			$('#align-right').addClass('chosen');
+			break;
+	};
+
+	// Turn textarea into formatted text
+	//$('#font-textarea').css('color', "#" + hexcolor);
 	$('#font-textarea').val(text);
-	var color = obj.fill.match(/\((\d+),(\d+),(\d+)\)/);
-	$('#color-selector').val(rgbToHex(color[1], color[2], color[3]));
+	$('#font-textarea').css('text-align', align);
+	$('#font-textarea').css('font-family', family);
+	$('#font-textarea').css('color', color);
+	$('#colorpicker').spectrum("set", color);
+
+
 };
-function rgbToHex(R,G,B) {
+/*function rgbToHex(R,G,B) {
 	return toHex(R)+toHex(G)+toHex(B)
 };
 function toHex(n) {
@@ -346,3 +516,11 @@ function toHex(n) {
  return "0123456789ABCDEF".charAt((n-n%16)/16)
       + "0123456789ABCDEF".charAt(n%16);
 };
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return "rgb(" + parseInt(result[1], 16) +", "+ parseInt(result[2], 16) +", "+ parseInt(result[3], 16) + ")";
+}*/
+function showEditor(){
+	$('#font-box').removeClass("hidden");
+	getFontInfo();
+}
