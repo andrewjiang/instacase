e20d8285f72269e47eea02845ef4aed8a509bd90
Zernyu Chou <swiftdemise@gmail.com>
2014-04-13 14:52:34 -0500

    Updated production env config

--- config/config.js
+++ config/config.js
@@ -9,13 +9,13 @@ module.exports = {
             name: 'Instacase'
         },
         facebook: {
-            clientID: '236858273165593',
-            clientSecret: '1a1c8584fd3ed34796f39ba5f826d4d7',
+            clientID: '305625832925093',
+            clientSecret: 'de26ad36634371a34381a5f5918ad182',
             callbackURL: 'http://localhost:3000/auth/facebook/callback'
         }
     },
     production: {
-        db: process.env.MONGOHQ_URL,
+        db: process.env.MONGO_URL,
         root: rootPath,
         app: {
             name: 'Instacase'
@@ -23,9 +23,9 @@ module.exports = {
         facebook: {
             clientID: '236858273165593',
             clientSecret: '1a1c8584fd3ed34796f39ba5f826d4d7',
-            callbackURL: 'http://instacase.herokuapp.com/auth/facebook/callback'
+            callbackURL: 'https://battletrophy.com/auth/facebook/callback'
         }
     },
-    test: {
+    stage: {
     }
 };
